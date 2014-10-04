b033c861bd426a342687e758e9cb2e45bb4bcc0a
mastermiw <mastermiw@192.168.7.30>
2014-10-04 13:46:05 +0200

    Incuidos ficheros de pruebas

---
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -0,0 +1,36 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+	private User u;
+
+	@Before 
+	public void before() { 
+		u = new User(1, "David", "Marchante"); 
+	} 
+
+	@Test 
+	public void testUserIntStringString() { 
+		assertEquals(1, u.getNumber() ); 
+		assertEquals("David", u.getName()); 
+		assertEquals("Marchante", u.getFamilyName()); 
+		
+	} 
+
+	@Test 
+	public void testFullName() { 
+		assertEquals("David Marchante", u.fullName());  
+	} 
+	
+	@Test 
+	public void testInitials() { 
+		assertEquals("D.", u.initials());  
+	} 
+	
+	 
+	
+}
